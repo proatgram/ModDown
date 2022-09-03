379d7bfd1f21a75daeb2cc9593e79108585187b1
Timothy Hutchins <tgh6407@gmail.com>
2022-09-03 15:32:12 -0500

    Added License info to files.

--- include/MinecraftDownload.h
+++ include/MinecraftDownload.h
@@ -1,4 +1,23 @@
 /*
+   This is a project that is dedicated to downloading Minecraft Mods from CurseForge using their API
+    Copyright (C) <2022>  Timothy Hutchins
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+/*
  * MinecraftDownload.h
  *
  *  Created on: Jun 12, 2022
